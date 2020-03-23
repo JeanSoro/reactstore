2fe1f0a3ca76f1a35d7df32daf1dac89eb93aa36
Jean <jean.soro007@gmail.com>
2020-03-23 10:31:54 -0400

    Styled Hero Component

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,5 +1,12 @@
 import React from "react";
 
 export default function About() {
-  return <h1>hello from about page</h1>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque ea corporis ipsa accusamus dicta placeat dolor consequatur, ad illum expedita consectetur suscipit architecto cum quod veniam quo quaerat. Fuga dolorem voluptate veritatis quo commodi perferendis doloremque tempore hic, laborum consectetur cumque incidunt sint dolore nihil praesentium blanditiis, facilis mollitia reprehenderit!
+      </p>
+    </section>
+  );
 }
--- src/pages/ErrorPage.js
+++ src/pages/ErrorPage.js
@@ -1,5 +1,13 @@
 import React from "react";
+import { Link } from 'react-router-dom';
 
 export default function ErrorPage() {
-  return <h1>hello form error page</h1>;
+  return (
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>404 NOT FOUND</h1>
+        <Link to="/" className="btn btn-primary">Back to Home</Link>
+      </div>
+    </section>
+  );
 }
