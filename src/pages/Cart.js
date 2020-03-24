e15f6a99fd6439f5375172f4a9729e32532c9a82
Jean <jean.soro007@gmail.com>
2020-03-24 10:15:11 -0400

    Style CartItem Component and attach event handlers to buttons

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,13 +1,36 @@
 import React, { useContext } from "react";
-import { CartContext } from '../context/cart'
+import { CartContext } from '../context/cart';
+import EmptyCart from '../components/Cart/EmptyCart';
+import CartItem from '../components/Cart/CartItem';
+import { Link } from 'react-router-dom';
+// import {UserContext} from '../context/user'
 
 const Cart = () => {
 
-  const { cart, total } = useContext(CartContext)
-  console.log({ cart, total })
+  let user = false;
+  const { cart, total } = useContext(CartContext);
+
+  if (cart.length === 0) {
+    return (
+      <EmptyCart />
+    );
+  }
 
 
-  return <h1>hello from cart page</h1>;
+  return (
+    <section className="cart-items section">
+      <h2>Your Cart</h2>
+      {cart.map(item => {
+        return <CartItem key={item.id} {...item} />
+      })}
+      <h2>Total: $ {total}</h2>
+      {user ?
+        <Link to="/checkout" className="btn btn-primary btn-block">Check Out Page</Link>
+        :
+        <Link to="/login" className="btn btn-primary btn-block">Login Page</Link>
+      }
+    </section>
+  );
 }
 
 export default Cart;
