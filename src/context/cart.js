2d0f22ab25bb0072e04ab09bd6ec5d9230f5f784
Jean <jean.soro007@gmail.com>
2020-03-24 10:41:45 -0400

    Initialized useEffect hook in cart context and implemented cart items and cart total functions

--- src/context/cart.js
+++ src/context/cart.js
@@ -15,9 +15,67 @@ const CartProvider = ({ children }) => {
   const [total, setTotal] = useState(0);
   const [cartItems, setCartItems] = useState(0);
 
+  useEffect(() => {
+    //localStorage
+
+    //1----------------- cart items-------------------------
+    let newCartItems = cart.reduce((accumulator, current) => {
+      return (accumulator += current.amount)
+    }, 0)
+    setCartItems(newCartItems);
+
+    //2----------------- cart Total----------------------------
+    let newCartTotal = cart.reduce((accumulator, current) => {
+      return accumulator += (current.amount * current.price);
+    }, 0)
+    newCartTotal = parseFloat(newCartTotal.toFixed(2));
+    setTotal(newCartTotal)
+
+  }, [cart])
+
+
+
+
+
+  //remove item
+  const removeItemFromCart = id => {
+
+  }
+  // **********************************************
+  // **********************************************
+  //increase item's amount
+  const increaseItemAmount = id => {
+
+  }
+  // **********************************************
+  // **********************************************
+
+  //decrease item's amount
+  const decreaseItemAmount = id => {
+
+  }
+  // **********************************************
+  // **********************************************
+  //add item to cart
+  const addItemToCart = product => {
+
+  }
+  // **********************************************
+  // **********************************************
+  //clear entire cart
+  const clearCart = () => {
+
+  }
+
 
   return (
-    <CartContext.Provider value={{ cart, total, cartItems }}>
+    <CartContext.Provider value={
+      {
+        cart, total, cartItems,
+        removeItemFromCart, increaseItemAmount,
+        decreaseItemAmount, addItemToCart, clearCart
+      }
+    }>
       {children}
     </CartContext.Provider>
   )
