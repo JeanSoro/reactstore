fb1544f42d4027ef7fb9a6f70ebb2de544e018a0
Jean <jean.soro007@gmail.com>
2020-03-31 15:23:31 -0400

    Final set up of backend using heroku as host and reinitialized strapi permissions

--- src/utils/URL.js
+++ src/utils/URL.js
@@ -1,6 +1,9 @@
 // url
 // export default "https://johnsmilgatutorials.com/projects/react-tech-store-v2";
 
-export default 'http://localhost:1337';
+// export default 'http://localhost:1337';
+
+export default 'https://hooks-store-api.herokuapp.com';
+
 
 
